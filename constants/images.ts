f3ea4d0a55af22d7d46be4c2da64fe4d95a98894
Efe G <98314362+gift56@users.noreply.github.com>
2024-08-22 12:12:34 -0700

    done working on changes

--- constants/images.ts
+++ constants/images.ts
@@ -3,6 +3,8 @@ const applePaymentImage = require("@/assets/images/applePaymentImage.png");
 const productImage1 = require("@/assets/images/productImage1.png");
 const productImage2 = require("@/assets/images/productImage4.png");
 const notFoundImage = require("@/assets/images/notFoundImage.png");
+const item1 = require("@/assets/images/item1.jpg");
+const item2 = require("@/assets/images/item2.jpg");
 
 export default {
   rydahLogo,
@@ -10,4 +12,6 @@ export default {
   productImage1,
   productImage2,
   notFoundImage,
+  item1,
+  item2,
 };
